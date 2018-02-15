48b8d99e91968050d551da974105b653a6d57afa
josemauricio2018 <36429512+josemauricio2018@users.noreply.github.com>
2018-02-15 11:32:11 -0500

    Cambios del 14 al 15 de febrero/2018.
    Cambios del 14 al 15 de febrero/2018.

---
+++ AppApplicants/AppApplicantsPresentation/Controllers/ApplicantsController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AppAplicantsBAL.Models;
+using AppApplicants.Models;
+
+namespace AppApplicantsPresentation.Controllers
+{
+    public class ApplicantsController : Controller
+    {
+        private IApplicants _repositoryApplicants;
+        private BusinessApplicants repositoryApplicants = new BusinessApplicants();
+
+        private IFile_Adjunts _repositoryFile_Adjunts;
+        private BusinessFile_Adjunts repositoryFile_Adjunts = new BusinessFile_Adjunts();
+
+        private IJobs _repositoryJobs;
+        private BusinessJob repositoryJobs = new BusinessJob();
+
+        //
+        // GET: /Applicants/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Create()
+        {
+            ApplicantView applicant = new ApplicantView();
+            applicant = FillJobs(applicant);
+
+            ViewBag.cjobs = applicant.sjobs;
+
+            return View(applicant);
+        }
+
+        //
+        // POST: /Applicants/Create
+
+        [HttpPost]
+        public ActionResult Create(ApplicantView applicant)
+        {
+            
+            return View(applicant);
+        }
+
+        public ApplicantView FillJobs(ApplicantView applicant)
+        {
+            //It is loaded combobox jobs.  
+            _repositoryJobs = repositoryJobs;
+
+            IList<Job> listJobs = _repositoryJobs.Consult_Job();
+            List<SelectListItem> listSelect = new List<SelectListItem>();
+
+            foreach (Job item in listJobs)
+            {
+                listSelect.Add(new SelectListItem() { Value = Convert.ToString(item.Consecutive), Text = item.Name });
+            }
+
+            applicant.sjobs = new SelectList(listSelect, "Value", "Text");
+
+            return applicant;
+        }
+
+
+    }
+}
---
+++ AppApplicants/AppApplicantsPresentation/Models/ApplicantView.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using AppApplicants.Models;
+
+namespace AppAplicantsBAL.Models
+{
+    public class ApplicantView : Applicant
+    {
+        public string Country_Code { get; set; }
+        public string Area_Code { get; set; }
+        public System.Web.Mvc.SelectList sjobs;
+    }
+}
