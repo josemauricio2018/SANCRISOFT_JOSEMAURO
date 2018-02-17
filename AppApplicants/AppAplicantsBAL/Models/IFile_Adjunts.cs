4e46560319fb0185874d6a3d455766a4bc6841e7
josemauricio2018 <mauricio.galeano2017@gmail.com>
2018-02-17 00:51:30 -0500

    Se adiciona la interfaz

---
+++ AppApplicants/AppAplicantsBAL/Models/IFile_Adjunts.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AppApplicants.Models;
+
+namespace AppAplicantsBAL.Models
+{
+    public interface IFile_Adjunts
+    {
+        Boolean Create_Files_Adjunt(int Id_Applicant, string Path_File_Local, string Path_File_URL, string Description_File);
+        Boolean Delete_File_Adjunts(int Consec_Applicant);
+        string GetMimeType(string FileName);
+        IEnumerable<Files_Adjunt> Consult_File_Adjunts(string Consec_Applicant);
+    }
+}
