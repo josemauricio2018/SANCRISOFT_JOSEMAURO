10c3666bbd0b281f5080fdb58a102fe5ef37b343
josemauricio2018 <mauricio.galeano2017@gmail.com>
2018-02-17 00:53:47 -0500

    Se adiciona el controlador

---
+++ AppApplicants/AppApplicantsPresentation/Controllers/Widget_File_Adjunts_UploadController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AppApplicantsPresentation.Controllers
+{
+    public class Widget_File_Adjunts_UploadController : Controller
+    {
+        //
+        // GET: /Widget_File_Adjunts_Upload/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
