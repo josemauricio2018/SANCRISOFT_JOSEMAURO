8735ea71ff4c1f0e3c4c3518fd7042ea970c261a
josemauricio2018 <mauricio.galeano2017@gmail.com>
2018-02-17 00:53:02 -0500

    Se adiciona la libreria de clases

---
+++ AppApplicants/AppApplicants/Models/ManageJobs.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AppApplicants.Models
+{
+    public class ManageJobs
+    {
+        private BD_APPLICANTSEntities3 dbApplicantContext = new BD_APPLICANTSEntities3();
+
+        public ManageJobs() { 
+        
+        }
+
+        /// <summary>
+        /// En este procedimiento se consultan los datos de todos los cargos.
+        /// </summary>
+        /// <returns></returns>
+        public IList<Job> Consult_Job()
+        {
+            IList<Job> lstJob = new List<Job>();
+
+            try
+            {
+                var consultJobs = dbApplicantContext.SP_CONSULT_JOB();
+
+                foreach (var item in consultJobs)
+                {
+                    lstJob.Add(new Job()
+                    {
+                        Consecutive = item.Consecutive,
+                        Name = item.Name_Job
+                    });
+                }
+            }
+            catch (Exception Ex)
+            {
+            }
+
+            return lstJob;
+        }
+
+    }
+}
