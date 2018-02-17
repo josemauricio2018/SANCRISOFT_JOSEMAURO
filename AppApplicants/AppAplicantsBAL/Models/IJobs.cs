db7ce33a0bf1ffd46a00e1d2a55c50a738561c3a
josemauricio2018 <mauricio.galeano2017@gmail.com>
2018-02-17 00:52:06 -0500

    Se adiciona la interfaz

---
+++ AppApplicants/AppAplicantsBAL/Models/IJobs.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AppApplicants.Models;
+
+namespace AppAplicantsBAL.Models
+{
+    public interface IJobs
+    {
+        IList<Job> Consult_Job();
+    }
+}
