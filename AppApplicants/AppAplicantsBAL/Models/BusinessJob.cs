160832c7277c2710c3f0039d64503ab1ba1b7f61
josemauricio2018 <mauricio.galeano2017@gmail.com>
2018-02-17 00:50:48 -0500

    Se adiciona la interfaz

---
+++ AppApplicants/AppAplicantsBAL/Models/BusinessJob.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using AppApplicants.Models;
+
+namespace AppAplicantsBAL.Models
+{
+    public class BusinessJob : IJobs
+    {
+        public BusinessJob() { 
+        
+        }
+
+        /// <summary>
+        /// En este procedimiento se habilita la consulta de los cargos.
+        /// </summary>
+        /// <returns></returns>
+        public IList<Job> Consult_Job()
+        {
+            IList<Job> lstJobs = null;
+
+            try
+            {
+                AppApplicants.Models.ManageJobs manageJobs = new AppApplicants.Models.ManageJobs();
+
+                lstJobs = manageJobs.Consult_Job();
+            }
+            catch (Exception Ex)
+            {
+            }
+
+            return lstJobs;
+        }
+
+    }
+}
