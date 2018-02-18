5fd62519155e4ca4a1d40fc7850ab46c1d9ad611
josemauricio2018 <mauricio.galeano2017@gmail.com>
2018-02-18 17:19:44 -0500

    Make changes to the Create procedure of type Get, add the Create procedure of type Post, add the procedure Edit of type Get and Post.

--- AppApplicants/AppApplicantsPresentation/Controllers/ApplicantsController.cs
+++ AppApplicants/AppApplicantsPresentation/Controllers/ApplicantsController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -34,6 +35,18 @@ namespace AppApplicantsPresentation.Controllers
 
             ViewBag.cjobs = applicant.sjobs;
 
+            ApplicantView applicantTemp = new ApplicantView();
+            applicantTemp.SessionId = HttpContext.Session.SessionID;
+
+            TempData["ModelApplicant"] = applicantTemp;
+
+            Session["SessionId"] = HttpContext.Session.SessionID;
+
+            //Consult list files adjunt to the apllicants
+            _repositoryFile_Adjunts = repositoryFile_Adjunts;
+            TempData["ReportFiles_Adjunt"] = _repositoryFile_Adjunts.Consult_File_Adjunt(applicant.Consecutive.ToString());
+
+
             return View(applicant);
         }
 
@@ -41,10 +54,110 @@ namespace AppApplicantsPresentation.Controllers
         // POST: /Applicants/Create
 
         [HttpPost]
-        public ActionResult Create(ApplicantView applicant)
+        public ActionResult Create(ApplicantView applicantView)
         {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    applicantView.PathPhotoLocal = "";
+                    applicantView.PathPhotoURL = "";
+
+                    _repositoryApplicants = repositoryApplicants;
+
+                    Boolean resultado = _repositoryApplicants.Create_Applicants(Convert.ToInt32(applicantView.ID_Job.ToString()),
+                    applicantView.Firts_Name,
+                    applicantView.Second_Name,
+                    applicantView.First_Last_Name,
+                    applicantView.Second_Last_Name,
+                    applicantView.Email,
+                    applicantView.International_Phone_Number,
+                    applicantView.Biography,
+                    applicantView.Birtday,
+                    applicantView.Street_Adress,
+                    applicantView.City,
+                    applicantView.Country,
+                    applicantView.Postal_Code,
+                    applicantView.PathPhotoLocal,
+                    applicantView.PathPhotoURL
+                    );
+                }
+            }
+            catch (Exception ex) { 
             
-            return View(applicant);
+            }
+
+            return View(applicantView);
+        }
+
+        //
+        // GET: /Clientes/Edit/5
+
+        public ActionResult Edit(int Consec_Applicant = 0)
+        {
+            ApplicantView applicantView = new ApplicantView();
+
+            _repositoryApplicants = repositoryApplicants;
+
+            IEnumerable<Report_Applicants> lstApplicant = _repositoryApplicants.Consult_Applicants(Consec_Applicant.ToString());
+           
+            return View(applicantView);
+        }
+
+        //
+        // POST: /Applicants/Edit
+
+        [HttpPost]
+        public ActionResult Edit(ApplicantView applicantView)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    _repositoryApplicants = repositoryApplicants;
+
+                    Boolean resultado = _repositoryApplicants.Edit_Applicants(applicantView.Consecutive,
+                    Convert.ToInt32(applicantView.ID_Job.ToString()),
+                    applicantView.Firts_Name,
+                    applicantView.Second_Name,
+                    applicantView.First_Last_Name,
+                    applicantView.Second_Last_Name,
+                    applicantView.Email,
+                    applicantView.International_Phone_Number,
+                    applicantView.Biography,
+                    applicantView.Birtday,
+                    applicantView.Street_Adress,
+                    applicantView.City,
+                    applicantView.Country,
+                    applicantView.Postal_Code,
+                    applicantView.PathPhotoLocal,
+                    applicantView.PathPhotoURL
+                    );
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+
+            return View(applicantView);
+        }
+
+        [HttpPost]
+        public ActionResult Upload_File_Adjunts(HttpPostedFileBase file)
+        {
+            // Verify that the user selected a file
+            if (file != null && file.ContentLength > 0)
+            {
+                // extract only the fielname
+                var fileName = Path.GetFileName(file.FileName);
+                // store the file inside ~/App_Data/uploads folder
+                var path = Path.Combine(Server.MapPath("~/App_Data/uploads"), fileName);
+                file.SaveAs(path);
+            }
+            // redirect back to the index action to show the form once again
+            return RedirectToAction("demo");
+            // return Content("successful");
         }
 
         public ApplicantView FillJobs(ApplicantView applicant)
@@ -64,7 +177,6 @@ namespace AppApplicantsPresentation.Controllers
 
             return applicant;
         }
-
 
     }
 }
