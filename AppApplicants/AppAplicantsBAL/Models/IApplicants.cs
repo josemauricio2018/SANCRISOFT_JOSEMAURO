1924637a1a037d09aed1a27e22ea62e1df44844f
josemauricio2018 <mauricio.galeano2017@gmail.com>
2018-02-18 17:13:24 -0500

    I was update the type of ienumerable list in the call procedure Consult_Applicants

--- AppApplicants/AppAplicantsBAL/Models/IApplicants.cs
+++ AppApplicants/AppAplicantsBAL/Models/IApplicants.cs
@@ -12,6 +12,6 @@ namespace AppAplicantsBAL.Models
         Boolean Create_Applicants(int Id_Job, string First_Name, string Second_Name, string First_Last_Name, string Second_Last_Name, string Email, string International_Phone_Number, string Biography, DateTime Birthday, string Street_Adress, string City, string Country, string Postal_Code, string Path_Photo_Local, string Path_Photo_URL);
         Boolean Edit_Applicants(int Consec_Applicant, int Id_Job, string First_Name, string Second_Name, string First_Last_Name, string Second_Last_Name, string Email, string International_Phone_Number, string Biography, DateTime Birthday, string Street_Adress, string City, string Country, string Postal_Code, string Path_Photo_Local, string Path_Photo_URL);
         Boolean Delete_Applicants(int Consec_Applicant);
-        IEnumerable<Applicant> Consult_Applicants(string Consec_Applicant);
+        IEnumerable<Report_Applicants> Consult_Applicants(string Consec_Applicant);
     }
 }
