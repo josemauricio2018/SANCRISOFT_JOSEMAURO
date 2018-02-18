ac8551902a29a750045516d55f74e971282f5251
josemauricio2018 <mauricio.galeano2017@gmail.com>
2018-02-18 17:05:34 -0500

    Add class report applicants for allow it adding the field Job_Name

---
+++ AppApplicants/AppApplicants/Models/Report_Applicants.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AppApplicants.Models
+{
+    public class Report_Applicants : Applicant
+    {
+        public string Job_Name { get; set; }
+    }
+}
