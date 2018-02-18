321a6fabe726079949f07a3bec1a9fa24822fa87
josemauricio2018 <mauricio.galeano2017@gmail.com>
2018-02-17 23:25:08 -0500

    Add new controller to showing the report applicants

---
+++ AppApplicants/AppApplicantsPresentation/Controllers/Report_ApplicantsController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AppApplicantsPresentation.Controllers
+{
+    public class Report_ApplicantsController : Controller
+    {
+        //
+        // GET: /Report_Applicants/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
