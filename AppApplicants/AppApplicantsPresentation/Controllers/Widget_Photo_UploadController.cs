a9798b6a40f95133992ff576c5ca6ee9d54fe3b4
josemauricio2018 <mauricio.galeano2017@gmail.com>
2018-02-17 23:23:36 -0500

    Add new controller for upload photo applicant

---
+++ AppApplicants/AppApplicantsPresentation/Controllers/Widget_Photo_UploadController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AppApplicantsPresentation.Controllers
+{
+    public class Widget_Photo_UploadController : Controller
+    {
+        //
+        // GET: /Widget_Photo_Upload/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
